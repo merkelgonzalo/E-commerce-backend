4d1dc53759c131f2582efef8c9c1a671806e874a
Gonzalo Merkel <merkelgonzalo.ok@gmail.com>
2023-09-17 22:36:54 -0300

    Deleted faker-js

--- src/routes/mockings.router.js
+++ src/routes/mockings.router.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { createProductsController } from "../controllers/mockings.controller.js";
+// import { createProductsController } from "../controllers/mockings.controller.js";
 import { autorization } from '../middlewares/autorization.js';
 
 const router = Router();
 
-router.post('/', /*autorization,*/ createProductsController);
+// router.post('/', /*autorization,*/ createProductsController);
 
 export default router;
